16e9eaeaf909c2e5d97225bd3f8c53f0f0baff98
NK <oscarfu0721@gmail.com>
2023-04-16 15:27:14 +0100

    use cath package

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -1,6 +1,8 @@
-import multer, { Multer } from "multer";
+import multer from "multer";
 import applyMiddleware from "../../utils/applyMiddleware";
 import { NextApiRequest, NextApiResponse } from "next/dist/shared/lib/utils";
+import { randomID } from "cath";
+
 export const config = {
   api: {
     bodyParser: false,
@@ -13,7 +15,7 @@ const upload = multer({
       cb(null, "./public/images");
     },
     filename: (req, file, cb) => {
-      cb(null, `${generateId(10)}.png`);
+      cb(null, `${randomID(10)}.png`);
     },
   }),
 }).single("file");
@@ -26,13 +28,4 @@ export default async function handler(
   res.status(200).send({
     url: `${process.env.NEXT_PUBLIC_HOST}/i/${req.file.filename.slice(0, -4)}`,
   });
-}
-
-function generateId(length: number) {
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var result = "", i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
 }
